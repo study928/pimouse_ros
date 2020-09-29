e5cafe4e11ab6379d45a5f47ac95e5c816e3901a
study study <study92810@gmail.com>
2020-09-29 06:05:54 +0000

    Add test file

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -34,16 +34,18 @@ class LightsensorTest(unittest.TestCase):
 
 		with open("/dev/rtlightsensors0", "w") as f:
 			f.write('-1 0 123 4321\n'
+                time.sleep(3)
 
-		time.sleep(3)
 		
 		self.assertFalse(self.count == 0, 'cannot subscribe the topic')
-		self.check_valuse(4231, 123, 0, -1)
+		self.check_valuses(4231, 123, 0, -1)
+
 	def test_change_parameter(self):
 		rospy.set_param('lightsensors_freq', 10)
 		time.sleep(2)
 		c_prev = self.count
-		time.sleep(3)
+                time.sleep(3)
+
 		
 		self.assertTrue(self.count < c_prev + 4, 'freq does not change')
 		self.assertFalse(self.count == c_prev, 'sbuscriber is stopped')
