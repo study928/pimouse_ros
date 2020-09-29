03376a2bf2cb440458a237f568ec6ff11e3ffa2d
study study <study92810@gmail.com>
2020-09-29 02:19:52 +0000

    Add

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -15,7 +15,7 @@ class BuzzerTest(unittest.TestCase):
 			pub.publish(1234)
 			time.sleep(0.1)
 
-		with open('/dev/rtbuzzer0', 'r') as f:
+		with open("/dev/rtbuzzer0", "r") as f:
 			data = f.readline()
 			self.assertEqual(data, '1234\n', 'value does not written to rtbuzzrt0')
 
